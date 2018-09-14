e5452774c09eee65de85615ff1ec09b55f6e1a40
Michael Maddalongit config --global user.email maddamic@berkeleyprep.org <you@example.com>
2018-09-14 08:32:20 -0400

    Complete

--- Homework1.java
+++ Homework1.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Michael Maddalon, D Block
  */
 
 import java.awt.Rectangle;
@@ -9,16 +9,22 @@ public class Homework1 {
 	/* Fix the error(s) in this method so that it runs properly
 	 */
 	public static int problem1() {
-		int mystery = 1;
+		int mystery = 2;
 		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
+		mystery = (1 - 2) * mystery;
 		return mystery;
 	}
+
 
 	/* Construct and return a Rectangle that has a width of w and a
 	 * height of h
 	 */
+
 	public static Rectangle problem2(int w, int h) {
+
+		Rectangle r = new Rectangle(0, 0, w, h);
+		return r;
+
 
 	}
 
@@ -26,17 +32,26 @@ public class Homework1 {
 	 */
 	public static int problem3() {
 
+		int prob3 = 5;
+		return prob3;
+
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
 
+		String str = new String("Computer Science");
+		return str;
+
+
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
+		num = num + 5;
+		return num;
 
 	}
 
